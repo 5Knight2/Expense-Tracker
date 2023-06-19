175d54c3cf9fabdc88854947ed432943fc4153e0
5Knight2 <pratiktarale258@gmail.com>
2023-06-19 11:06:01 +0530

    backend with delete and edit

--- controller/expense.js
+++ controller/expense.js
@@ -3,7 +3,7 @@ const  Sequelize  = require('sequelize');
 
 
 exports.get=(req,res,next)=>{
-
+    console.log('get');
 Expense.findAll()
 .then((expense)=>{
     console.log(expense)
@@ -13,18 +13,63 @@ Expense.findAll()
 
 
 }
+exports.getById=(req,res,next)=>{
+    
+    console.log('get');
+    Expense.findByPk(req.params.id)
+    .then((expense)=>{
+        res.json(expense)
+    })
+    .catch((err)=>{console.log(err)})
+    
+}
 
 
 exports.post=(req,res,next)=>{
-    console.log(req);
+    
+    console.log('post');
+    if(req.body.id==''){
     Expense.create({
         amount:req.body.amount,
         type:req.body.type,
         description:req.body.description})
         .then((result)=>{
             console.log(result)
-            res.json()
-        })
+            res.json(result)
+        })}else{
+
+            Expense.findByPk(req.body.id)
+    .then((expense)=>{
+        expense.amount=req.body.amount;
+        expense.type=req.body.type
+        expense.description=req.body.description
+        return expense.save();
+    }).then(result=> {console.log('hii')
+    console.log(result)
+    return res.end()
+    })
+
+  .catch(err=>{console.log(err)})
+
+    .catch((err)=>{console.log(err)})
+
+        }
     
 }
 
+exports.delete_prod=(req,res,next)=>{
+console.log('delete')
+const id=Number(req.params.id);
+    Expense.findByPk(id)
+    .then((expense)=>{
+        
+        return expense.destroy()
+    }).then((response)=>{
+        
+        return res.end();
+      })
+ 
+    .catch((err)=>{console.log(err)})
+    
+    
+    }
--- router/expense.js
+++ router/expense.js
@@ -4,9 +4,11 @@ const expense_controller=require('../controller/expense');
 
 router.get('/expense',expense_controller.get);
 
+router.get('/expense/:id',expense_controller.getById);
+
 router.post('/expense',expense_controller.post);
 
-router.get('/expense/:id',expense_controller.get);
+router.get('/delete/:id',expense_controller.delete_prod);
 
 module.exports=router;
 
